6abd3cd6d64d27f455926ff252a096ab8ec033fb
KamchybekovMarat <marat.kamchybekov01@mail.ru>
2021-01-19 19:00:37 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo, BookStore
+
+
+def homepage(request):
+    return render(request, "index.html")
+
+
+def test(request):
+    todo_list = ToDo.objects.all()
+    return render(request, 'test.html', {"todo_list": todo_list})
+
+
+def second(request):
+    return HttpResponse('test 2 page')
+
+
+def bookStore(request):
+    book_list = BookStore.objects.all()
+    return render(request, 'books.html', {"book_list": book_list})
+
+
+def add_todo(request):
+    form = request.POST
+    text = form['todo_text']
+    todo = ToDo(text=text)
+    todo.save()
+
+    return redirect(test)
+
+
+def book_add(request):
+    return render(request, 'books-add.html')
+
+
+def add_book(request):
+    form = request.POST
+    bookstor = BookStore(title=form['book-title'], subtitle=form['book-subtitle'],
+                         description=form['book-description'], price=form['book-price'], genre=form['book-genre'], author=form['book-author'], year=form['book-year'])
+    bookstor.save()
+
+    return redirect(bookStore)
---
+++ todo/urls.py
@@ -0,0 +1,32 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from main.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', homepage, name='home'),
+    path('test/', test, name='test'),
+    path('test2/', second, name='test2'),
+    path('book-store/', bookStore, name='bookstore'),
+    path('add-todo/', add_todo, name='add-todo'),
+    path('book-add/', book_add, name='book-add'),
+    path('add-book', add_book, name='book-add-on-the-site')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
